c73c89192d567c6638eb3865b28fbf175c185062
BinLucky <yavuztrx@gmail.com>
2021-11-23 01:13:09 +0300

    SignUp

--- packages/authentication_repository/lib/src/authentication_repository.dart
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -2,37 +2,67 @@ import 'package:authentication_repository/src/models/models.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/cupertino.dart';
 import 'package:cache/cache.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthenticationRepository{
-  AuthenticationRepository({CacheClient? cache,
-  firebase_auth.FirebaseAuth? firebaseAuth,
-  GoogleSignIn? googleSignIn}):_cache = cache?? CacheClient(),
-_firebaseAuth ? firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
-_googleSignIn =googleSignIn ?? GoogleSignIn.standard();
+class AuthenticationRepository {
+  AuthenticationRepository(
+      {CacheClient? cache,
+      firebase_auth.FirebaseAuth? firebaseAuth,
+      GoogleSignIn? googleSignIn})
+      : _cache = cache ?? CacheClient(),
+        _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
 
-final CacheClient _cache;
-final firebase_auth.FirebaseAuth _firebaseAuth;
-final GoogleSignIn _googleSignIn;
+  final CacheClient _cache;
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
 
-@visibleForTesting
-static const userCacheKey = '__user_cache_key__';
+  @visibleForTesting
+  static const userCacheKey = '__user_cache_key__';
 
-Stream<User> get user {
-  return _firebaseAuth.authStateChanges().map((event) {
-final user = event == null ? User.empty : event.toUser;
-_cache.write(key:userCacheKey, value:user);
-return user;
-  });}
+  Stream<User> get user {
+    return _firebaseAuth.authStateChanges().map((event) {
+      final user = event == null ? User.empty : event.toUser;
+      _cache.write(key: userCacheKey, value: user);
+      return user;
+    });
+  }
 
-User get currentUser{
-  return _cache.read<User>(key: userCacheKey)??User.empty;
+  User get currentUser {
+    return _cache.read<User>(key: userCacheKey) ?? User.empty;
+  }
+
+  Future<void> singUp({required email, required password}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on firebase_auth.FirebaseAuthException catch (e) {
+    } catch (_) {}
+    ;
+  }
+
+  Future<void> logInWithGoogle() async {
+    try {
+      late final firebase_auth.AuthCredential credential;
+      if (kIsWeb) {
+        final googleProvider = firebase_auth.GoogleAuthProvider();
+        final userCredential =
+            await _firebaseAuth.signInWithPopup(googleProvider);
+        credential = userCredential.credential!;
+      } else {
+        final googleUser = await _googleSignIn.signIn();
+        final googleAuth = await googleUser!.authentication;
+        credential = firebase_auth.GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+      }
+    } catch (e) {}
+    ;
+  }
 }
 
-
-}
-extension on firebase_auth.User{
-  User get toUser{
-    return User(id:uid,email: email,name: displayName);
+extension on firebase_auth.User {
+  User get toUser {
+    return User(id: uid, email: email, name: displayName);
   }
 }
